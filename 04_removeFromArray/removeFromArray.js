876659623d48fe720090f6ed510f9d15709cd150
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-23 14:43:37 +0100

    Make partial solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removed) {
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === removed){
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
