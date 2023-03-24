b8c635c8e3474fdbe3ebc79c2c9a5b462376c9ab
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-24 10:47:08 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    if (first < 0
+        || last < 0
+        || typeof(first) != ("number" || "bigint")
+        || typeof(last) != ("number" || "bigint") ){
+        return "ERROR";
+    }
+
+    let smaller = first;
+    let larger = last;
+
+    if (first > last){
+        smaller = last;
+        larger = first;
+    }
+    
+    let sum = larger;
+    while (larger !== smaller){
+        larger--;
+        sum = sum + larger;
+    }
+    return sum;
 };
 
 // Do not edit below this line
