1f84b41ad59d6c753f78c796a3d31eff299bcceb
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-24 11:37:24 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = (temperature - 32) * 5/9;
+  return Number(temperature.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = temperature * 9/5 + 32;
+  return Number(temperature.toFixed(1));
 };
+
+
+console.log( convertToCelsius(100) );
+if (convertToCelsius(100) === 37.8){
+  console.log("true");
+}
 
 // Do not edit below this line
 module.exports = {
