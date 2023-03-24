5dc6a7ef273f28104cd7d9454f1c48b445350628
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-24 10:15:21 +0100

    Make possible solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,16 @@
-const removeFromArray = function(array, removed) {
-    for (let i = 0; i < array.length; i++){
-        if (array[i] === removed){
-            array.splice(i, 1);
-            i--;
+const removeFromArray = function(array, ...removed) {
+    for (let j = 0; j < arguments[arguments.length - 1]; j++){
+        for (let i = 0; i < array.length; i++){
+            if (array[i] === arguments[j]){
+                array.splice(i, 1);
+                i--;
+            }
         }
     }
     return array;
 };
 
+console.log( removeFromArray([0, 1, 2, 3, 4], 3, 4) );
+
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
