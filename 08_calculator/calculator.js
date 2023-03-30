23d7bfe207b3b322d4082d3d97bb449a1e9c0119
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-03-30 15:06:45 +0200

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,57 @@
-const add = function() {
-	
+const add = function(number1, ...numbers) {
+  let result = number1;
+  for (let i = 0; i < numbers.length; i++){
+    result = result + numbers[i];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, ...numbers) {
+  let result = number1;
+  for (let i = 0; i < numbers.length; i++){
+    result = result - numbers[i];
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(number1, ...numbers) {
+  let result = number1;
+  for (let i = 0; i < numbers.length; i++){
+    result = result + numbers[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(number1, ...numbers) {
+  let result = number1;
+  for (let i = 0; i < numbers.length; i++){
+    result = result * numbers[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  if (power === 0){
+    return 1;
+  }
+
+  let result = base;
+  for (let i = 1; i < power; i++){
+    result = result * base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+function factorial(number) {
+  if (number === 0){
+    return 1;
+  }
+  let result = number;
+  for (let i = 1; i < number; i++){
+    result = result * (number - i);
+  }
+  return result;
 };
 
-// Do not edit below this line
-module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+console.log(factorial(10));
